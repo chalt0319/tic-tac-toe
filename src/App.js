c8673cf2ad97b6e3c046806fbc34591aa063a52e
Courtney <chalt0319@gmail.com>
2018-11-20 14:29:14 -0500

    add board component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/containers/board'
+
 
 class App extends Component {
   render() {
@@ -8,6 +10,9 @@ class App extends Component {
       <div className="App">
         <header>
           Welcome to Tic Tac Toe!!
+          <div>
+            <Board />
+          </div>
         </header>
       </div>
     );
---
+++ src/components/containers/board.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+  render() {
+
+    return (
+      <img src="http://2.bp.blogspot.com/-U2ssg3mi0yA/VXtkE4h_9JI/AAAAAAAAAEU/wVS2xO9YptU/s1600/1.gif" alt="board" />
+    );
+  }
+}
+
+export default Board
